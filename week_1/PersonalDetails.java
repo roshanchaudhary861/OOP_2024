2da9b8287336c5e9503e32a9faf218ad366ff736
roshanchaudhary861 <149452941+roshanchaudhary861@users.noreply.github.com>
2024-02-27 19:57:33 +0545

    PersonalDetails

---
+++ week_1/PersonalDetails.java
@@ -0,0 +1,16 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        // Personal details
+        String name = "roshan";
+        String address = "Kathmandu";
+        int age = 21;
+        String phoneNumber = "9814865861";
+
+        // Output personal details to the screen
+        System.out.println("Personal Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Age: " + age);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+}
