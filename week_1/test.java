860676e659f1b1c02b1d254d8032226642385795
roshanchaudhary861 <149452941+roshanchaudhary861@users.noreply.github.com>
2024-02-27 20:05:00 +0545

    test

---
+++ week_1/test.java
@@ -0,0 +1,17 @@
+public class test
+{
+   //----------------------------------------------------
+   //  Prints a statement.
+   //----------------------------------------------------
+   public static void main (String[] args)
+   // public static void man (String[] args)
+   {
+      System.out.println ("An Emergency Broadcast");
+      // System.out.println ("An emergency Broadcast");
+      // System.out.println (An emergency Broadcast");
+      // System.out.bogus (An emergency Broadcast");
+      // system.out.bogus (An emergency Broadcast")
+      // system.out.bogus (An emergency Broadcast")
+
+   }
+}
