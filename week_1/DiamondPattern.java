7d49d905f0f8b30571e3b3bf759def913ccb1e01
roshanchaudhary861 <149452941+roshanchaudhary861@users.noreply.github.com>
2024-02-27 20:01:10 +0545

    DiamondPattern

---
+++ week_1/DiamondPattern.java
@@ -0,0 +1,17 @@
+// DiamondPattern.java
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        System.out.println("Diamond Pattern:");
+        System.out.println("   *    ");
+        System.out.println("  ***   ");
+        System.out.println(" *****  ");
+        System.out.println("******* ");
+        System.out.println(" *****  ");
+        System.out.println("  ***   ");
+        System.out.println("   *    ");
+
+
+
+    }
+}
