f56bcd3cf9ba29812fca1400d5833e3b75fe7482
roshanchaudhary861 <149452941+roshanchaudhary861@users.noreply.github.com>
2024-02-29 18:00:22 +0545

    StudentGradeTable

---
+++ week_2/StudentGradeTable.java
@@ -0,0 +1,18 @@
+public class StudentGradeTable {
+
+    public static void main(String[] args) {
+
+
+
+        // Print table header
+        System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==          Student Points          ==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+        System.out.println("\nName            Lab     Bonus   Total");
+        System.out.println("\n----            ----    ----    ----");
+        System.out.println("\njoe              39       50       39");
+        System.out.println("\nwilliam          50       8        58");
+        System.out.println("\nmarysue          39       10       49");
+        System.out.println("----            ---     -----   -----");
+    }
+}
